482d04a1e91ff5cab466c149c870132fa54bf66e
Vegoz <109350234+Vegoz@users.noreply.github.com>
2024-03-31 22:17:31 +0300

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function showMessage(response) {
 
     // Update text content and hide name message
     document.getElementById("question").textContent =
-      "ця кнопка не працює";
+      "idi nahuy";
     document.getElementById("name").style.display = "none";
 
     // Optional: You can also add a timeout to reset the position after a few seconds
